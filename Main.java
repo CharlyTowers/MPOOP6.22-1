3a3c5aa0202b58d3c751c663cc3a897b321b95c1
CarlosTorres26 <>
2021-10-21 19:27:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+@author Carlos Torres
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es: "+suma(2,2));
+  }
+  /**
+  *@param a Primer entero de la suma
+  *@param b Segundo entero de la suma
+  *@return La suma de los enteros a + b
+  */
+  public static int suma(int a, int b){
+    return a+b;
+  }
+}
